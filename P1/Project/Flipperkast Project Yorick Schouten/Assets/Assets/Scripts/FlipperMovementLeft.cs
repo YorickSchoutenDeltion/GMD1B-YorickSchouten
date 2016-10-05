6011d25a2e4da1bd2da1dddad52efdd87e7a6dd4
YorickSchoutenDeltion <97045778@st.deltion.nl>
2016-10-05 20:55:40 +0200

    Added functional flippers and changed some physics settings

---
+++ P1/Project/Flipperkast Project Yorick Schouten/Assets/Assets/Scripts/FlipperMovementLeft.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipperMovementLeft : MonoBehaviour {
+
+    private bool pressed = false;
+    private object motor;
+    
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+    void FixedUpdate ()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            HingeJoint hinge = GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.force = 450;
+            motor.targetVelocity = -1800;
+            motor.freeSpin = false;
+            hinge.motor = motor;
+            hinge.useMotor = true;
+        }
+        else
+        {
+            HingeJoint hinge = GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.force = 250;
+            motor.targetVelocity = 1500;
+            motor.freeSpin = false;
+            hinge.motor = motor;
+            hinge.useMotor = true;
+        }
+
+
+        }
+    }
+
---
+++ P1/Project/Flipperkast Project Yorick Schouten/Assets/Assets/Scripts/FlipperMovementRight.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipperMovementRight : MonoBehaviour
+{
+
+    private bool pressed = false;
+    private object motor;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            HingeJoint hinge = GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.force = 450;
+            motor.targetVelocity = 1800;
+            motor.freeSpin = false;
+            hinge.motor = motor;
+            hinge.useMotor = true;
+        }
+        else
+        {
+            HingeJoint hinge = GetComponent<HingeJoint>();
+            JointMotor motor = hinge.motor;
+            motor.force = 250;
+            motor.targetVelocity = -1500;
+            motor.freeSpin = false;
+            hinge.motor = motor;
+            hinge.useMotor = true;
+        }
+
+
+    }
+}
+
