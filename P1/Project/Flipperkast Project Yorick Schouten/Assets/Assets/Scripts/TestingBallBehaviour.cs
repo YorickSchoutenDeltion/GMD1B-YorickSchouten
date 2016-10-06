5a62d2b3296cd572deaf2712d6525e7fb146d083
YorickSchoutenDeltion <97045778@st.deltion.nl>
2016-10-06 11:37:10 +0200

    Added force to ball, working on force based on buttonpress

---
+++ P1/Project/Flipperkast Project Yorick Schouten/Assets/Assets/Scripts/TestingBallBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingBallBehaviour : MonoBehaviour {
+
+    public float force;
+    private float counter = 5;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Jump"))
+        {
+            force = counter + counter / Time.deltaTime;
+        }
+
+	
+	}
+
+    void OnTriggerStay ()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            transform.rotation = Quaternion.identity;
+            GetComponent<Rigidbody>().AddForce(transform.forward * force);
+        }
+    }
+}
